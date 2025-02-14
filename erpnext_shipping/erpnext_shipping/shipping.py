fd2896ee34f5e04cf63b5e14b3387348901c2f5b
Marc-Constantin Enke <marc@alyf.de>
2025-02-14 13:10:35 +0100

    feat: phone and parcel validation

--- erpnext_shipping/erpnext_shipping/shipping.py
+++ erpnext_shipping/erpnext_shipping/shipping.py
@@ -14,6 +14,8 @@ from erpnext_shipping.erpnext_shipping.utils import (
 	get_address,
 	get_contact,
 	match_parcel_service_type_carrier,
+	validate_parcels,
+	validate_phone,
 )
 
 
@@ -38,6 +40,8 @@ def fetch_shipping_rates(
 	delivery_address = get_address(delivery_address_name)
 	parcels = json.loads(parcels)
 
+	validate_parcels(parcels)
+
 	if letmeship_enabled:
 		pickup_contact = None
 		delivery_contact = None
@@ -46,6 +50,8 @@ def fetch_shipping_rates(
 		else:
 			pickup_contact = get_company_contact(user=pickup_contact_name)
 			pickup_contact.email_id = pickup_contact.pop("email", None)
+			phone = pickup_contact.get("phone")
+			validate_phone(phone)
 
 		delivery_contact = get_contact(delivery_contact_name)
 
--- erpnext_shipping/erpnext_shipping/utils.py
+++ erpnext_shipping/erpnext_shipping/utils.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2020, Frappe Technologies and contributors
 # For license information, please see license.txt
+import re
+
 import frappe
 from frappe import _
 from frappe.utils.data import get_link_to_form
@@ -44,6 +46,21 @@ def validate_address(address):
 
 	if not address.pincode or address.pincode.strip() == "":
 		frappe.throw(_("Please add a valid pincode in Address {0}.").format(address.address_title))
+
+
+def validate_parcels(parcels):
+	for parcel in parcels:
+		if parcel.get("length", 0) < 1:
+			frappe.throw(_("Parcel length must be greater than 1."))
+		if parcel.get("width", 0) < 1:
+			frappe.throw(_("Parcel width must be greater than 1."))
+		if parcel.get("height", 0) < 1:
+			frappe.throw(_("Parcel height must be greater than 1."))
+
+
+def validate_phone(phone):
+	if not re.match(r"^\+(?!0)\d+$", phone):
+		frappe.throw(_("Company contact phone must start with '+' and contain up to 4 digits in the prefix."))
 
 
 def get_country_code(country_name):
