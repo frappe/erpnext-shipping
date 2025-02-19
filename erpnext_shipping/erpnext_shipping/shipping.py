222d3eb54a3a279f4772bd82ca186477f6b937b2
barredterra <14891507+barredterra@users.noreply.github.com>
2025-02-19 21:30:37 +0100

    fix: pase JSON first
    (cherry picked from commit b2caef91ba5440cf34d9e8e696d1f5d83319bac6)

--- erpnext_shipping/erpnext_shipping/shipping.py
+++ erpnext_shipping/erpnext_shipping/shipping.py
@@ -97,7 +97,9 @@ def create_shipment(
 	delivery_contact_name=None,
 	delivery_notes=None,
 ):
-	# Create Shipment for the selected provider
+	if isinstance(delivery_notes, str):
+		delivery_notes = json.loads(delivery_notes)
+
 	if delivery_notes is None:
 		delivery_notes = []
 
@@ -213,6 +215,9 @@ def save_label_as_attachment(shipment: str, content: bytes) -> str:
 
 @frappe.whitelist()
 def update_tracking(shipment, service_provider, shipment_id, delivery_notes=None):
+	if isinstance(delivery_notes, str):
+		delivery_notes = json.loads(delivery_notes)
+
 	if delivery_notes is None:
 		delivery_notes = []
 
@@ -237,9 +242,6 @@ def update_tracking(shipment, service_provider, shipment_id, delivery_notes=None
 def update_delivery_note(delivery_notes, shipment_info=None, tracking_info=None):
 	# Update Shipment Info in Delivery Note
 	# Using db_set since some services might not exist
-	if isinstance(delivery_notes, str):
-		delivery_notes = json.loads(delivery_notes)
-
 	delivery_notes = list(set(delivery_notes))
 
 	for delivery_note in delivery_notes:
